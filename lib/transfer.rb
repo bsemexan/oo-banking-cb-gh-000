e7dbc85eefcd3c73db973f62bbdc283e001d0524
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 05:48:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if valid? && @receiver.balance == amount || @status=="complete"
+    if valid? && @receiver.balance == amount "
       @sender.deposit(amount)
       @receiver.balance -= amount
       @status = "reversed"
